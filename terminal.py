1e933e17c6058292d2c7cca9c3dcd4ec24fa7f43
hctpv <142475920+hctpv@users.noreply.github.com>
2023-08-17 02:46:55 -0300

    Add files via upload

---
+++ terminal.py
@@ -0,0 +1,27 @@
+#Creacion de wifiHacking for android model
+import colorama
+from colorama import Fore, Style, Back
+from colorama import just_fix_windows_console
+just_fix_windows_console()
+
+print(Fore.RED,"""
+                            ,-.                               
+       ___,---.__          /'|`\          __,---,___          
+    ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.       
+  ,'        |           ~'\     /`~           |        `.      
+ /      ___//              `. ,'          ,  , \___      \    
+|    ,-'   `-.__   _         |        ,    __,-'   `-.    |    
+|   /          /\_  `   .    |    ,      _/\          \   |   
+\  |           \ \`-.___ \   |   / ___,-'/ /           |  /  
+ \  \           | `._   `\\  |  //'   _,' |           /  /      
+  `-.\         /'  _ `---'' , . ``---' _  `\         /,-'     
+     ``       /     \    ,='/ \`=.    /     \       ''          
+             |__   /|\_,--.,-.--,--._/|\   __|                  
+             /  `./  \\`\ |  |  | /,//' \,'  \                  
+            /   /     ||--+--|--+-/-|     \   \                 
+           |   |     /'\_\_\ | /_/_/`\     |   |                
+            \   \__, \_     `~'     _/ .__/   /            
+             `-._,-'   `-._______,-'   `-._,-'
+""")
+
+print(Fore.RED, "Testeo Terminal")
